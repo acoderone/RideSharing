d57639d5843dbec3d5d55b2fd26a249b4e7c2bc0
Acoderone <avi5200786@gmail.com>
2025-11-14 01:44:16 +0530

    updated models

--- DriverService/src/main/java/com/Driver/DriverService/model/Rider.java
+++ DriverService/src/main/java/com/Driver/DriverService/model/Rider.java
@@ -1,26 +1,36 @@
 package com.Driver.DriverService.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Getter
-@Setter
 public class Rider {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String name;
+
+    // Never expose this field in API responses
+    @JsonIgnore
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false, unique = true)
     private String license;
+
     @Enumerated(EnumType.STRING)
-    private Status status=Status.AVAILABLE;
+    @Column(nullable = false)
+    private Status status = Status.AVAILABLE;
 
 
 }
--- RideService/src/main/java/com/rideService/RideService/model/Ride.java
+++ RideService/src/main/java/com/rideService/RideService/model/Ride.java
@@ -7,17 +7,35 @@ import lombok.Data;
 @Data
 public class Ride {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
     private String rideId;
+
+    @Column(nullable = false)
     private Double pickupLongitude;
+
+    @Column(nullable = false)
     private Double pickupLatitude;
+
+    @Column(nullable = false)
     private Double destinationLongitude;
+
+    @Column(nullable = false)
     private Double destinationLatitude;
+
+    @Column(nullable = false)
     private Long userId;
+
+    @Column(nullable = false)
     private Long riderId;
+
+    @Column(nullable = false)
     private Double ridePrice;
+
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Status status;
 
 
